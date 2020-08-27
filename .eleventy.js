d4e4e2ffe784e51218b2f5bfa909c08561d8eb19
rickhocutt <rick.hocutt@gmail.com>
2020-08-26 21:03:27 -0400

    Add CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,23 @@
-module.exports = function (eleventyConfig) {
-
-  eleventyConfig.addPassthroughCopy('assets');
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('images');
+  eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('admin');
-}
+
+  const {
+    DateTime
+  } = require("luxon");
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+      return DateTime.fromJSDate(dateObj, {
+        zone: 'utc'
+      }).toFormat('yy-MM-dd');
+    });
+
+    eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat("dd-MM-yy");
+  });
+
+};
--- posts/my-first-post.md
+++ posts/my-first-post.md
@@ -2,6 +2,7 @@
 layout: layouts/posts.njk
 title: My first blog post
 date: 2020-08-26
+featuredImage: /images/uploads/image1.jpg
 ---
 
 Here is the body of the post.
