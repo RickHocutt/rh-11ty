af22416574c55c73fdc1f14594d4ff44e775660c
rickhocutt <rick.hocutt@gmail.com>
2020-09-12 12:56:40 -0400

    Swap CSS framework to Tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,27 @@
 module.exports = function(eleventyConfig) {
   // eleventyConfig.setUseGitIgnore(false);
 
+  eleventyConfig.addWatchTarget("./_tmp/style.css");
+
+  eleventyConfig.addPassthroughCopy('favicon.ico');
   eleventyConfig.addPassthroughCopy('images');
-  eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('scripts');
   eleventyConfig.addPassthroughCopy('admin');
 
-  const {
-    DateTime
-  } = require("luxon");
+  eleventyConfig.addShortcode("version", function () {
+    return String(Date.now());
+  });
+
+  const { DateTime } = require("luxon");
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
-    });
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('yy-MM-dd');
+  });
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
+  eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {
       zone: 'utc'
     }).toFormat("dd-MM-yy");
