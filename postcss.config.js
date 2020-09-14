e0bc06f9166dc725b78359be0d4ffd25a4b68831
rickhocutt <rick.hocutt@gmail.com>
2020-09-13 22:32:54 -0400

    add purgecss to postcss, typography enhancements

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,18 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        'index.html',
+        'resume.html',
+        'src/**/*.html',
+        'src/scripts/*.js',
+        '_includes/**/*.njk',
+        'portfolio/**/*.md',
+        'posts/**/*.md'
+      ],
+      defaultExtractor: content => content.match(/[A-za-z0-9-_:/]+/g)
+    })
   ],
 };
